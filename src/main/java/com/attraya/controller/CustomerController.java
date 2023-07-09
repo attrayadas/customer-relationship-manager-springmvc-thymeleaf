62730433b33681d270273d40f1273d043f28340d
Attraya Das <attrayaghoshdas@gmail.com>
2023-07-09 15:48:23 +0530

    Add customer-form.jsp

--- src/main/java/com/attraya/controller/CustomerController.java
+++ src/main/java/com/attraya/controller/CustomerController.java
@@ -5,6 +5,8 @@ import com.attraya.service.ICustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -25,6 +27,19 @@ public class CustomerController {
         customers.forEach(System.out::println);
         model.put("customers", customers);
         return "list-customers";
+    }
+
+    @GetMapping("/showForm")
+    public String showFormForAdd(Map<String, Object> model){
+        Customer customer = new Customer();
+        model.put("customer", customer);
+        return "customer-form";
+    }
+
+    @PostMapping("/saveCustomer")
+    public String saveCustomer(@ModelAttribute("customer") Customer customer){
+        System.out.println(customer);
+        return "redirect:/customer/list";
     }
 
 }
--- src/main/webapp/WEB-INF/pages/list-customers.jsp
+++ src/main/webapp/WEB-INF/pages/list-customers.jsp
@@ -17,6 +17,9 @@
 	</div>
 	<div id="container">
 		<div id="content">
+		    <input type='button' value='Add Customer'
+        				onclick="window.location.href='showForm'; return false;"
+        				class="add-button" />
 			<table>
 				<tr>
 					<th>First Name</th>
