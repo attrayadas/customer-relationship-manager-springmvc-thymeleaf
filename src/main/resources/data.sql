7dce4163d66f67f1522a02f26a04b6878cd4e064
Attraya Das <attrayaghoshdas@gmail.com>
2023-07-11 21:15:58 +0530

    Load initial data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into CUSTOMER(EMAIL, FIRST_NAME, LAST_NAME) values ('vindiesel@gmail.com', 'Vin', 'Diesel');
+insert into CUSTOMER(EMAIL, FIRST_NAME, LAST_NAME) values ('keanureeves@gmail.com', 'Keanu', 'Reeves');
+insert into CUSTOMER(EMAIL, FIRST_NAME, LAST_NAME) values ('johnnydepp@gmail.com', 'Johnny', 'Depp');
+insert into CUSTOMER(EMAIL, FIRST_NAME, LAST_NAME) values ('tomcruise@gmail.com', 'Tom', 'Cruise');
+insert into CUSTOMER(EMAIL, FIRST_NAME, LAST_NAME) values ('emmawatson@gmail.com', 'Emma', 'Watson');
+insert into CUSTOMER(EMAIL, FIRST_NAME, LAST_NAME) values ('hughjackman@gmail.com', 'Hugh', 'Jackman');
